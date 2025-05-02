6b9c3710d6f52ca79ea72e5b22e01d444d260f71
Maycon Reis <maycon.jesus@grupomulti.com.br>
2025-05-02 16:59:11 -0300

    feat: adds app footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-footer color="background">
+    <v-container class="d-flex align-center justify-center">
+      <span class="mr-2">&copy;</span>
+      <span class="mr-2">{{ new Date().getFullYear() }}</span>
+      <span>Maycon Reis</span>
+    </v-container>
+  </v-footer>
+</template>
