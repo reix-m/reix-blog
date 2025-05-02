524c9cd69d0f977f7bd7f5eb4a7854ea8a9eec04
Maycon Reis <maycon.jesus@grupomulti.com.br>
2025-05-02 17:00:24 -0300

    feat: adds app config

---
+++ app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+  mail: 'reix-m@pm.me',
+  state: 'SP',
+  city: 'São Paulo',
+  links: {
+    github: 'https://github.com/reix-m',
+    linkedin: 'https://linkedin.com/in/maycon-rjesus',
+  },
+});
