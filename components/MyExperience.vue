733d5092432c16b5b7111fa0a32328d019879b1b
Maycon Reis <maycon.jesus@grupomulti.com.br>
2025-05-08 20:17:55 -0300

    feat: adds my experience component

---
+++ components/MyExperience.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-sheet color="transparent">
+    <div class="d-flex align-center">
+      <v-container>
+        <v-row>
+          <v-col cols="auto d-flex align-center">
+            <img
+              src="https://multilaser.vtexassets.com/arquivos/logo-multi-header-novo.svg"
+              :width="80"
+              class="bg-white pa-3 rounded"
+            />
+          </v-col>
+          <v-col class="d-flex flex-column justify-center">
+            <div class="d-flex gap-1 align-center justify-space-between">
+              <span class="text-subtitle-1 font-weight-bold text-white">Multi</span>
+              <span class="text-subtitle-2 font-weight-bold text-white">Jan 2024 - Presente</span>
+            </div>
+            <span class="text-subtitle-2">Desenvolvedor Backend</span>
+          </v-col>
+        </v-row>
+        <v-row class="px-4">
+          <ul>
+            <li class="text-body-1">
+              Desenvolvi e mantive o sistema responsável pela criação e controle de RMAs e Devoluções.
+            </li>
+            <li class="text-body-1">
+              Trabalhei no desenvolvimento de serviços para integração com ERP SAP, utilizando arquitetura baseada em
+              Eventos.
+            </li>
+            <li class="text-body-1">
+              Desenvolvi bibliotecas Npm utilizadas internamente para facilitar a criação de APIs Node.js e NestJS.
+            </li>
+            <li class="text-body-1">
+              Liderei revisões de código por pares e orientei aprendizes, promovendo uma cultura de colaboração e
+              melhoria contínua dentro da equipe.
+            </li>
+            <li class="text-body-1">
+              Atuei no desenvolvimento e treinamento da equipe para implantação de pipelines de CI/CD do Gitlab.
+            </li>
+          </ul>
+        </v-row>
+      </v-container>
+    </div>
+  </v-sheet>
+</template>
