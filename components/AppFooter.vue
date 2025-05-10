3fd87cab022c085c9559fd4eb10ceeeefe7f3614
Reix <reix-m@pm.me>
2025-05-10 15:22:08 -0300

    feat: adds logo in navbar and footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,6 +1,7 @@
 <template>
   <v-footer color="background">
     <v-container class="d-flex align-center justify-center">
+      <img src="~/assets/image/logo.png" :width="30" class="mx-4" />
       <span class="mr-2">&copy;</span>
       <span class="mr-2">{{ new Date().getFullYear() }}</span>
       <span>Maycon Reis</span>
--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -1,23 +1,28 @@
 <template>
   <v-app-bar color="background" flat>
-    <v-container class="d-flex align-center justify-end">
-      <v-list class="d-flex align-center" bg-color="transparent">
-        <v-list-item
-          v-for="(item, index) in menuItems"
-          :key="index"
-          :to="item.to"
-          link
-          variant="plain"
-          density="compact"
-          class="d-flex align-center justify-center"
-          color="fontPrimary"
-        >
-          <v-list-item-title class="d-flex align-center">
-            <v-icon :icon="item.icon" v-if="item.icon" class="mx-2" />
-            <span v-if="item.title">{{ item.title }}</span>
-          </v-list-item-title>
-        </v-list-item>
-      </v-list>
+    <v-container class="d-flex align-center justify-space-between">
+      <div class="d-flex align-center justify-center">
+        <img src="~/assets/image/logo.png" :width="30" />
+      </div>
+      <div class="d-flex align-center justify-end">
+        <v-list class="d-flex align-center" bg-color="transparent">
+          <v-list-item
+            v-for="(item, index) in menuItems"
+            :key="index"
+            :to="item.to"
+            link
+            variant="plain"
+            density="compact"
+            class="d-flex align-center justify-center"
+            color="fontPrimary"
+          >
+            <v-list-item-title class="d-flex align-center">
+              <v-icon :icon="item.icon" v-if="item.icon" class="mx-2" />
+              <span v-if="item.title">{{ item.title }}</span>
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </div>
     </v-container>
   </v-app-bar>
 </template>
